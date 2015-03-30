df3fa7108234025ad053634b85f8e3db26652156
Dominic McAdden <dominic.mcadden@gmail.com>
2015-03-30 14:39:40 +0100

    project setup: simple html page, added empty files for css and js, put some funky regex into my build file to point my html page to the minified version once copied over to the build folder, added build folder to gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -13,22 +13,23 @@ gulp.task('clean', function() {
 // process scripts
 gulp.task('scripts', function() {
 	return gulp.src('src/js/*.js')
-		.pipe(plugins.concat('main.js'))
+		.pipe(plugins.concat('app.js'))
 		.pipe(plugins.rename({suffix: '.min'}))
 		.pipe(plugins.uglify())
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest('build/js/'));
 });
 
 // copy html
 gulp.task('html', function() {
 	return gulp.src('src/*.html')
+		.pipe(plugins.replace(/(\w*)(\.js)/g, '$1.min$2'))
 		.pipe(gulp.dest('build/'));
 });
 
 // copy css
 gulp.task('css', function() {
 	return gulp.src('src/css/*.*')
-		.pipe(gulp.dest('build/'));
+		.pipe(gulp.dest('build/css/'));
 });
 
 // watch
@@ -39,7 +40,7 @@ gulp.task('watch', function() {
 });
 
 // compound tasks
-gulp.task('local', ['clean','scripts','css','html']);
+gulp.task('local', ['clean', 'scripts', 'html', 'css']);
 
 // default
 gulp.task('default', ['local']);
