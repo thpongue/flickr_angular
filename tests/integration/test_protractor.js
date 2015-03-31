602bee05a07616e58fd964e2bc968de95251eb37
Dominic McAdden <dominic.mcadden@gmail.com>
2015-03-31 20:30:32 +0100

    added the earliest requirements into the placeholder integration test file

--- tests/integration/test_protractor.js
+++ tests/integration/test_protractor.js
@@ -1,6 +1,21 @@
 describe('placeholder integration test', function() {
-	it('should work if you start the server first', function() {
+	it('call the flickr API and display a list of results as text', function() {
 		browser.get('http://localhost:8000');
+		it('should show a photo url', function() {
+
+		});
+		it('should show a post title', function() {
+
+		});
+		it('should show a post author', function() {
+
+		});
+		it('should show a publish date', function() {
+
+		});
+		it('should show a flickr link', function() {
+
+		});
 		expect(true).toBe(true);
 	});
 });
