2758d470f9b3e3ebf3077494ec35ea9965f95942
Dominic McAdden <dominic.mcadden@gmail.com>
2015-04-01 14:24:15 +0100

    getting used to protractor... hardcoding some html (rather than making a real data call) while I work out how this works

--- protractor.config.js
+++ protractor.config.js
@@ -2,7 +2,7 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: [
-		'test/integration/**/*.js'
+		'tests/integration/**/*.js'
 	],  
   capabilities: {
     browserName: 'chrome'
--- tests/integration/list_view_spec.js
+++ tests/integration/list_view_spec.js
@@ -1,20 +1,22 @@
 describe('minimal list view spec', function() {
-	it('should call the flickr API and display a list of results as text', function() {
-		browser.get('http://localhost:8000');
+	describe('should call the flickr API and display at least one post', function() {
+		beforeEach(function() {
+			browser.get('http://localhost:8000');
+		});
 		it('should show a photo url', function() {
-
+				expect(element(by.id('postList.post1.photoUrl')).isPresent()).toBe(true);
 		});
 		it('should show a post title', function() {
-
+				expect(element(by.id('postList.post1.postTitle')).isPresent()).toBe(true);
 		});
 		it('should show a post author', function() {
-
+				expect(element(by.id('postList.postAuthor')).isPresent()).toBe(true);
 		});
 		it('should show a publish date', function() {
-
+				expect(element(by.id('postList.post1.publishDate')).isPresent()).toBe(true);
 		});
 		it('should show a flickr link', function() {
-
+				expect(element(by.id('postList.post1.flickrLink')).isPresent()).toBe(true);
 		});
 	});
 });
