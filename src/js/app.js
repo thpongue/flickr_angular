f08692219023a8cfa11b2334106cacc440bf56a0
Dominic McAdden <dominic.mcadden@gmail.com>
2015-04-02 12:59:30 +0100

    externalised script

---
+++ src/js/app.js
@@ -0,0 +1,78 @@
+angular.module('app', []);
+
+(function() {
+	angular.module('app').controller('listController', function($scope) {
+		$scope.items = [
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			},
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			},
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			},
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			},
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			},
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			},
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			},
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			},
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			},
+			{
+				photoUrl: 'dummy data',
+				postTitle: 'dummy data',
+				postAuthor: 'dummy data',
+				publishDate: 'dummy data',
+				flickrLink: 'dummy data'
+			}
+		];
+	});
+}());
