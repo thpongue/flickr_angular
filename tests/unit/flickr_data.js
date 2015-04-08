21adce08e26e38bb9ef8676a6a42edd743a0c931
Dominic McAdden <dominic.mcadden@gmail.com>
2015-04-08 10:30:50 +0100

    changed initial unit test to look a little more like the actual code I'm writing

--- tests/unit/flickr_data.js
+++
@@ -1,9 +0,0 @@
-describe('placeholder unit test', function() {
-	var sut;
-	beforeEach(function() {
-		sut = new FlickrData();
-	});
-	it('should run this', function() {
-		expect(sut).not.toBe(null);
-	});
-});
