ab0d318433253182f183d88fe5d78266988d2ce4
Dominic McAdden <dominic.mcadden@gmail.com>
2015-04-10 09:21:27 +0100

    tinkering with spec - added a parent node representing the whole product and changed describe to xits for pending specs

--- tests/integration/flickr_viewer.js
+++ tests/integration/flickr_viewer.js
@@ -1,47 +1,49 @@
-describe('Item view spec', function() {
-	beforeEach(function() {
-		browser.get('http://localhost:8001');
-		element(by.css('#loadDataButton')).click();
-	});
+describe('Flickr viewer', function() {
+	describe('Item view spec', function() {
+		beforeEach(function() {
+			browser.get('http://localhost:8001');
+			element(by.css('#loadDataButton')).click();
+		});
 
-	it('should have a button to load the data', function() {
-		expect(element(by.css('#loadDataButton')).isPresent()).toBe(true);
-	});
+		it('should have a button to load the data', function() {
+			expect(element(by.css('#loadDataButton')).isPresent()).toBe(true);
+		});
 
-	describe('should show the word \'loading\' whilst the data is loading', function() {
+		xit('should show the word \'loading\' whilst the data is loading', function() {
+			
+		});
 		
-	});
-	
-	describe('should show the word \'failed\' if the data fails to load', function() {
+		xit('should show the word \'failed\' if the data fails to load', function() {
+			
+		});
 		
-	});
-	
-	describe('should list the data for 10 flickr posts', function() {
-		var numberOfItems = 10;
-		it('should show a photo url for each item', function() {
-			for (i=1; i<=numberOfItems;i++) {
-				expect(element(by.css('#items #item'+ i +' #media')).isPresent()).toBe(true);
-			}
-		});
-		it('should show a post title for each item', function() {
-			for (i=1; i<=numberOfItems;i++) {
-				expect(element(by.css('#items #item'+ i +' #title')).isPresent()).toBe(true);
-			}
-		});
-		it('should show a post author for each item', function() {
-			for (i=1; i<=numberOfItems;i++) {
-				expect(element(by.css('#items #item'+ i +' #author')).isPresent()).toBe(true);
-			}
-		});
-		it('should show a publish date for each item', function() {
-			for (i=1; i<=numberOfItems;i++) {
-				expect(element(by.css('#items #item'+ i +' #date_taken')).isPresent()).toBe(true);
-			}
-		});
-		it('should show a flickr link for each item', function() {
-			for (i=1; i<=numberOfItems;i++) {
-				expect(element(by.css('#items #item'+ i +' #link')).isPresent()).toBe(true);
-			}
+		describe('should list the data for 10 flickr posts', function() {
+			var numberOfItems = 10;
+			it('should show a photo url for each item', function() {
+				for (i=1; i<=numberOfItems;i++) {
+					expect(element(by.css('#items #item'+ i +' #media')).isPresent()).toBe(true);
+				}
+			});
+			it('should show a post title for each item', function() {
+				for (i=1; i<=numberOfItems;i++) {
+					expect(element(by.css('#items #item'+ i +' #title')).isPresent()).toBe(true);
+				}
+			});
+			it('should show a post author for each item', function() {
+				for (i=1; i<=numberOfItems;i++) {
+					expect(element(by.css('#items #item'+ i +' #author')).isPresent()).toBe(true);
+				}
+			});
+			it('should show a publish date for each item', function() {
+				for (i=1; i<=numberOfItems;i++) {
+					expect(element(by.css('#items #item'+ i +' #date_taken')).isPresent()).toBe(true);
+				}
+			});
+			it('should show a flickr link for each item', function() {
+				for (i=1; i<=numberOfItems;i++) {
+					expect(element(by.css('#items #item'+ i +' #link')).isPresent()).toBe(true);
+				}
+			});
 		});
 	});
 });
