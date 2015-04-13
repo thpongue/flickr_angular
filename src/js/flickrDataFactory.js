5e43269f024dcfba6a4e2e2a8a181c4debd684a9
Dominic McAdden <dominic.mcadden@gmail.com>
2015-04-13 09:44:42 +0100

    made my factory a proper factory

--- src/js/flickrDataFactory.js
+++ src/js/flickrDataFactory.js
@@ -2,13 +2,15 @@
 	'use strict';
 	angular
 		.module('app')
-			.service('flickrDataFactory', flickrDataFactory);
+			.factory('flickrDataFactory', flickrDataFactory);
 
 	// returns a FlickrData object which it populates once the data has arrived
 	function flickrDataFactory($http) {
-		this.http = $http;
-		this.flickrUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK';
-		this.getData = getData;
+		return {
+			http: $http,
+			flickrUrl: 'https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK',
+			getData: getData
+		}
 		
 		// private 
 		function getData() {
